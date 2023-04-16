3f29949960c2b879d3011b78eb5b9edd17572320
XDKaren <k7hu@ucsd.edu>
2023-04-16 14:50:21 -0700

    modify dparetoR function

--- homeworks/R/dparetoR.R
+++ homeworks/R/dparetoR.R
@@ -1,13 +1,53 @@
 
 
-dparetoR<-function (x, alpha, beta, log = FALSE) 
-{
-  if (log == FALSE){
-    return (beta*alpha^beta/x^(beta+1))
-  } 
-  else{
-    (log(beta*alpha^beta/x^(beta+1)))
+# dparetoR <- function(x, alpha, beta) {
+#   
+#   if (x < alpha) {
+#     return(0)
+#   } else {
+#     return( beta * alpha^beta / x^(beta + 1))
+#   }
+# }
+
+paretodens <- function(x, alpha, beta) {
+  if (x < alpha) {
+    return (0)
+  } else {
+    return(beta * alpha^beta / x^(beta + 1))
   }
-  
 }
 
+dparetoR <- function(x, alpha, beta, logd = FALSE) {
+  xn <- length(x)
+  alphan <- length(alpha)
+  betan <- length(beta)
+  
+  if (xn == 0L || alphan == 0L || betan == 0L) {
+    cat(sprintf("Zero length detected: xn = %d, alphan = %d, betan = %d\n", xn, alphan, betan))
+  }
+  
+  n <- max(xn, alphan, betan)
+  f <- numeric(n)
+  
+  ix <- 1
+  ialpha <- 1
+  ibeta <- 1
+  for (i in 1:n) {
+    if (logd == FALSE) {
+      f[i] <- paretodens(x[ix], alpha[ialpha], beta[ibeta])
+    } else {
+      f[i] <- log(paretodens(x[ix], alpha[ialpha], beta[ibeta]))
+    }
+    
+    ix <- ix + 1
+    if (ix > xn) ix <- 1
+    
+    ialpha <- ialpha + 1
+    if (ialpha > alphan) ialpha <- 1
+    
+    ibeta <- ibeta + 1
+    if (ibeta > betan) ibeta <- 1
+  }
+  
+  return(f)
+}
