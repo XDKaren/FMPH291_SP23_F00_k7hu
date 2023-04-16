0106baf115e4eaa8c29909ee80b0924f3836ab85
XDKaren <k7hu@ucsd.edu>
2023-04-16 14:52:28 -0700

    modify dpareto function by threeVector function

--- homeworks/Rcpp/dpareto.cpp
+++ homeworks/Rcpp/dpareto.cpp
@@ -2,31 +2,47 @@
 using namespace Rcpp;
 // [[Rcpp::export]]
 double paretodens(double x, double alpha, double beta) {
-  if (x < beta) {
+  if (x < alpha) {
     return 0.0;
   } else {
-    return alpha * pow(beta, alpha) / pow(x, alpha + 1.0);
+    return beta * pow(alpha, beta) / pow(x, beta + 1.0);
   }
 }
   
 
+
 // [[Rcpp::export]]
 NumericVector dpareto(NumericVector x, NumericVector alpha, NumericVector beta, bool logd = false) {
-  int n = x.size();
+  int xn = x.size();
+  int alphan = alpha.size();
+  int betan = beta.size();
+  
+    
+    
+  if (xn == 0L || alphan == 0L || betan == 0L) {
+    Rprintf("Zero length detected: xn = %d, alphan = %d, betan = %d\n", xn, alphan, betan);
+  }
+    
+    
+  int n = std::max(std::max(xn, alphan), betan);
   NumericVector f(n);
     
+  int ix = 0, ialpha = 0, ibeta = 0;
   for (int i = 0; i < n; ++i) {
-    if (x[i] < beta[i]) {
-      f[i] = 0.0;
-    } else {
-      if (logd == true) {
-        f[i] = log(paretodens(x[i], alpha[i], beta[i]));
-      } else {
-        f[i] = paretodens(x[i], alpha[i], beta[i]);
-      }
+    
+    if (logd == false) {
+      f[i]=paretodens(x[ix], alpha[ialpha], beta[ibeta]);
     }
+    
+
+    else {
+      f[i]=log(paretodens(x[ix], alpha[ialpha], beta[ibeta]));
+    }
+      
+    if (++ix == xn) ix = 0;
+    if (++ialpha == alphan) ialpha = 0;
+    if (++ibeta == betan) ibeta = 0;
   }
     
   return f;
-}
-  
+  }
--- homeworks/Rcpp/threeVectors.cpp
+++ homeworks/Rcpp/threeVectors.cpp
@@ -1,34 +1,34 @@
-// #include <Rcpp.h>
-// using namespace Rcpp;
-// // Demonstrate how to `Vectorize' 2 arguments.
-// // Print the indexes and the element indexed.
-// // `void' type needs no `return' statement
-// // Rprintf is described in Writing R Extensions Section 6.5 Printing
-// void print2Values( int i, int ix, int iy, double xelt, double yelt) {
-//   Rprintf("i = %d ix = %d x element = %f iy = %d y element = %f\n",
-//           i, ix, xelt, iy, yelt);
-// }
-// // [[Rcpp::export]]
-// CharacterVector twoVectors(
-//     NumericVector x, NumericVector y) {
-//   int xn = x.size();
-//   int yn = y.size();
-//   // if (xn == 0 || yn == 0) special handling is needed such as
-//   // returning a zero length vector
-//   if (xn==0L || yn == 0L)
-//     Rprintf(" Zero Length Detected: xn == %d, yn ==%d\n", xn, yn);
-//   int n = xn > yn ? xn : yn;
-//   
-//   int ix=0, iy=0;
-//   for (int i = 0; i<n; ++i) {
-//     // Do something here:
-//     print2Values( i, ix, iy, x[ix], y[iy] );
-//     // Update the indexes for the next pass:
-//     if (++ix == xn ) ix = 0 ;
-//     if (++iy == yn ) iy = 0;
-//   }
-//   return "Done";
-// }
+#include <Rcpp.h>
+using namespace Rcpp;
+// Demonstrate how to `Vectorize' 2 arguments.
+// Print the indexes and the element indexed.
+// `void' type needs no `return' statement
+// Rprintf is described in Writing R Extensions Section 6.5 Printing
+void print2Values( int i, int ix, int iy, double xelt, double yelt) {
+  Rprintf("i = %d ix = %d x element = %f iy = %d y element = %f\n",
+          i, ix, xelt, iy, yelt);
+}
+// [[Rcpp::export]]
+CharacterVector twoVectors(
+    NumericVector x, NumericVector y) {
+  int xn = x.size();
+  int yn = y.size();
+  // if (xn == 0 || yn == 0) special handling is needed such as
+  // returning a zero length vector
+  if (xn==0L || yn == 0L)
+    Rprintf(" Zero Length Detected: xn == %d, yn ==%d\n", xn, yn);
+  int n = xn > yn ? xn : yn;
+
+  int ix=0, iy=0;
+  for (int i = 0; i<n; ++i) {
+    // Do something here:
+    print2Values( i, ix, iy, x[ix], y[iy] );
+    // Update the indexes for the next pass:
+    if (++ix == xn ) ix = 0 ;
+    if (++iy == yn ) iy = 0;
+  }
+  return "Done";
+}
 // /*** R
 // twoVectors(1:2,3:7)
 // */
@@ -63,6 +63,7 @@ CharacterVector threeVectors(NumericVector x, NumericVector y, NumericVector z) 
   int ix = 0, iy = 0, iz = 0;
   for (int i = 0; i < n; ++i) {
     
+
     print3Values(i, ix, iy, iz, x[ix], y[iy], z[iz]);
     
     
