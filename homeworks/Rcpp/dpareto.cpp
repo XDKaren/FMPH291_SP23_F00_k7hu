3f8711ec43aee89870413ae41c77ac8ea215a09f
XDKaren <k7hu@ucsd.edu>
2023-04-15 18:48:41 -0700

    modify paretodens function and create depareto function that is vectorizeds

--- homeworks/Rcpp/dpareto.cpp
+++ homeworks/Rcpp/dpareto.cpp
@@ -1,13 +1,32 @@
-#include <math.h>
+#include <Rcpp.h>
+using namespace Rcpp;
 // [[Rcpp::export]]
-double paretodens(double x, double alpha, double beta, bool use_log=false){
-  double res = (beta*pow(alpha,beta)/pow(x,beta+1)); 
-  if (use_log == false){
-    return res;
+double paretodens(double x, double alpha, double beta) {
+  if (x < beta) {
+    return 0.0;
   } else {
-    return log(res);
+    return alpha * pow(beta, alpha) / pow(x, alpha + 1.0);
   }
 }
   
-  
+
+// [[Rcpp::export]]
+NumericVector dpareto(NumericVector x, NumericVector alpha, NumericVector beta, bool logd = false) {
+  int n = x.size();
+  NumericVector f(n);
+    
+  for (int i = 0; i < n; ++i) {
+    if (x[i] < beta[i]) {
+      f[i] = 0.0;
+    } else {
+      if (logd == true) {
+        f[i] = log(paretodens(x[i], alpha[i], beta[i]));
+      } else {
+        f[i] = paretodens(x[i], alpha[i], beta[i]);
+      }
+    }
+  }
+    
+  return f;
+}
   
